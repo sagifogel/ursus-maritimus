1d711d72afa3626969ff1eb7f501c74bb86ff846
Sagi <sagi.fogel@gmail.com>
2018-10-20 02:42:47 +0300

    integrating the ReaderStorage into the EventsHttpService

--- src/EventsHttpServer.hs
+++ src/EventsHttpServer.hs
@@ -2,20 +2,37 @@
 
 module EventsHttpServer (httpService) where
 
-import Web.Scotty
+import Data.Map
 import AppConfig
+import Data.Text
+import Web.Scotty
+import Text.Printf
+import EventReaderStorage
+import Control.Monad.Trans
+import Data.String (fromString)
 
-httpService :: IO ()
-httpService = do
+httpService :: ReaderStorage -> IO ()
+httpService readerStorage = do
   conf <- loadConfig 
   let port' = either (const 3000) (port . getServerConfig) conf
-  scotty port' $ root >> eventsRoute
+  scotty port' $ root >> eventsByTypeRoute readerStorage >> eventsByDataRoute readerStorage
   return () 
-  
-eventsRoute :: ScottyM ()  
-eventsRoute = get "/eventsbytype" $ do
-  html "Events counter"
   
 root :: ScottyM ()  
 root = get "/" $ do
   html "Welcome to the event counter service!"
+
+eventsByTypeRoute :: ReaderStorage -> ScottyM ()  
+eventsByTypeRoute readerStorage = get "/eventsbytype" $ do
+  let events = getEventCountByType readerStorage
+  text <- lift $ fmap (foldEvents "Event type: %s, Count: %s") events
+  html $ fromString text 
+
+eventsByDataRoute :: ReaderStorage -> ScottyM ()  
+eventsByDataRoute readerStorage = get "/eventsbydata" $ do
+  let events = getEventCountByData readerStorage
+  text <- lift $ fmap  (foldEvents "Event data: %s, Count: %s") events
+  html $ fromString text
+
+foldEvents :: String -> Map String Int -> String
+foldEvents fmt = foldrWithKey (\k v acc -> printf fmt k (show v)) ""
